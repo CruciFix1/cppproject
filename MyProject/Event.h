1572453d75cbf813cb00d241b8afc11c42bcd1db
Stanciu Cristian <stanciucristian21@stud.ase.ro>
2022-12-04 20:11:42 +0200

    updated version of event class

--- MyProject/Event.h
+++ MyProject/Event.h
@@ -1,5 +1,21 @@
-#pragma once
+#ifndef EVENT_H
+#define EVENT_H
+
 class Event
 {
+private:
+	char* name;
+	int date;
+	int time;
+
+public:
+	void setName(char* nume);
+	void setDate(int data);
+	void setTime(int timp);
+
+	char* getName();
+	int getDate();
+	int getTime();
 };
 
+#endif 
