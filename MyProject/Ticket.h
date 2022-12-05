f5deb64eda4127f0582a528eaf5533f9bc50571a
Stanciu Cristian <stanciucristian21@stud.ase.ro>
2022-12-05 22:31:23 +0200

    Updated the Ticket header. Added generic method and overloads for increment operator ! operator and equals operator

--- MyProject/Ticket.h
+++ MyProject/Ticket.h
@@ -16,14 +16,22 @@ public:
 
 	Ticket();
 	Ticket(char* uniqueID, int row, int seat);
+
+
 	Ticket(const Ticket& copy);
+	Ticket& operator=(const Ticket& copy);
 	~Ticket();
 
 	friend ostream& operator<<(ostream& out, Ticket a);
 	friend istream& operator>>(istream& in, Ticket& a);
 
+	Ticket& operator++();
+	Ticket operator++(int);
+	bool operator!();
+
 	static void setNrofgentickets(int nrtichete);
 	static void incrementTickets();
+	void neighbours(Ticket a);
 
 
 	static int getNrofgentickets();
